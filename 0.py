3137e1d43883943c8b44911411e7b04226c6405c
Lintang Wisesa <lintangwisesa@ymail.com>
2023-11-06 11:38:35 +0700

    variable

--- 0.py
+++ 0.py
@@ -1,1 +1,3 @@
 print("Hello World")
+# ini kode untuk menampilkan Hello World
+# shortcut untuk comment/uncomment: CTRL + /
---
+++ 1_var.py
@@ -0,0 +1,15 @@
+# variabel: containers for storing data value
+FirstName = "Lintang"           # PascalCase
+lastName = 'Wisesa'             # camelCase
+latest_age = 30                 # snake_case
+# LATEST_AGE = 30               # snake_case
+# home-address = "Jakarta"      # kebab-case
+isMarried = True
+isHavingKids = False
+height = 175.23
+
+print(FirstName)
+print(FirstName, lastName)
+print(f"Halo nama saya {FirstName} {lastName}")
+
+
