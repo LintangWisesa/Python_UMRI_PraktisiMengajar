0e4aa0e4d555a8ce7c3d144d485bf3bc1e23062d
Lintang Wisesa <lintangwisesa@ymail.com>
2023-11-14 10:48:33 +0700

    list method

--- 6_list.py
+++ 6_list.py
@@ -9,4 +9,22 @@ print(angka[24])
 # start:stop:step
 print(angka[:5])         # akses elemen ke 0-4
 print(angka[0:5])        # akses elemen ke 0-4
-print(angka[0:5:1])      # akses elemen 0, 2, 4
+print(angka[0:5:2])      # akses elemen 0, 2, 4
+
+buah = ["Apel", "Belimbing", "Ceri", "Duku"]
+print(buah)
+buah.append("Jambu")    # add element at the end
+print(buah)
+buah.insert(1, "Semangka")  # add element at certain index (index = 1)
+buah.insert(3, "Melon")  # add element at certain index (index = 3)
+print(buah)
+buah.remove("Duku")     # remove Duku from the list
+print(buah)
+buah.pop()              # remove last element
+buah.pop(0)             # remove element index - 0
+print(buah)
+
+buah.sort()             # sort ascending alphabetically / numerically 
+print(buah)
+buah.sort(reverse=True) # sort descending
+print(buah)
