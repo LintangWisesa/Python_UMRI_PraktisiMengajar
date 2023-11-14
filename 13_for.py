eccd4ecdb59d8ecf33402f504b2530bec473f3c2
Lintang Wisesa <lintangwisesa@ymail.com>
2023-11-14 12:35:28 +0700

    for loop

---
+++ 13_for.py
@@ -0,0 +1,22 @@
+# LOOP: perulangan
+
+x = [1, 2, 3, 4, 5]
+for i in x:
+    print(i)
+
+y = "banana"
+for i in y:
+    print(i)
+
+# function RANGE(start, stop, step)
+z = range(5)
+print(z)
+print(list(z))
+a = range(0,5,1)
+print(list(a))
+b = range(0,10,2)
+print(list(b))
+
+# show angka 1-10
+for i in range(1,10001,1):
+    print(i)
