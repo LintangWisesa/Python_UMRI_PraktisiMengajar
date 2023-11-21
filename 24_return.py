7bec772e0de716425b07e46dfc06b41fed86c0aa
Lintang Wisesa <lintangwisesa@ymail.com>
2023-11-21 11:56:57 +0700

    return func

---
+++ 24_return.py
@@ -0,0 +1,19 @@
+# Return Function
+# function yg mereturn/memiliki value
+
+def halo():
+    print("Halo ~")
+halo()
+print(halo())
+
+def hai():
+    print("Hai ~")
+    return "Hai"
+print(hai())
+
+# func dengan return value
+x = 12
+print(x)
+def x():
+    return 120
+print(x())
