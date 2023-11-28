772860a06a96b3b1690eaa01f0a12b8da26fe29c
Lintang Wisesa <lintangwisesa@ymail.com>
2023-11-28 12:10:39 +0700

    oop

---
+++ 32_oop.py
@@ -0,0 +1,28 @@
+# OOP: object & class
+# object = hasil cetakan dari class
+# class = template / blueprint / cetakan
+
+# class dengan 1 property data: x=5 & 1 property func: y()
+class ContohClass:
+    x = 5                           # property - data
+    def kaliSeratus(self):          # property - func (method)
+        return self.x * 100
+
+# object z from class ContohClass
+z = ContohClass()
+print(z)
+print(z.x)
+print(z.kaliSeratus())
+
+# cetakan jelly
+class CetakanJelly():
+    warna = 'cokelat'
+    rasa = 'cokelat'
+    def addTopping(self):
+        return 'Jelly Cokelat Meses'
+
+# create object
+a = CetakanJelly()
+print(a.warna, a.rasa, a.addTopping())
+b = CetakanJelly()
+print(a.warna, a.rasa, a.addTopping())
---
+++ 33_static.py
@@ -0,0 +1,15 @@
+
+# cetakan jelly
+class CetakanJelly():
+    warna = 'cokelat'
+    rasa = 'cokelat'
+    def addTopping(self):
+        return 'Jelly Cokelat Meses'
+
+# create object
+a = CetakanJelly()
+print(a.warna, a.rasa, a.addTopping())
+b = CetakanJelly()
+print(b.warna, b.rasa, b.addTopping())
+c = CetakanJelly()
+print(c.warna, c.rasa, c.addTopping())
